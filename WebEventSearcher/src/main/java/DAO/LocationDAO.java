fbe3d6b07aa0f2ebfd6317fa068c1fac4f57ec22
Seajer <jersi28@gmail.com>
2015-12-29 00:30:23 +0200

    clear and recreate folders

--- WebEventSearcher/src/main/java/DAO/LocationDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import java.util.List;
-
-public interface LocationDAO<T> {
-	
-	public List<T> findByAddress(String address);
-
-}
--- WebEventSearcher/src/main/java/DAOImpl/EventTypeDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package DAOImpl;
-
-import DAO.EntityDAOAbstract;
-import DAO.EventTypeDAO;
-import Entity.EventType;
-
-public class EventTypeDAOImpl extends EntityDAOAbstract<EventType, Integer> implements EventTypeDAO<EventType>{
-
-}
